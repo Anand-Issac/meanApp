f5a176a156e542599dd3e57bdd44c9049d9dec99
Anand-Issac <42297577+Anand-Issac@users.noreply.github.com>
2019-07-30 16:08:04 -0400

    Add files via upload
    Pre Angular HTTP client implement

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Simple post message app
+# MeanCourse
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.1.
 
--- backend/app.js
+++ backend/app.js
@@ -5,14 +5,23 @@ const app = express();
 const port = '3000';
 app.set('port', port);
 
-app.use( (req, res, next) => {
-  console.log('first middleware');
-  next();
-});
-
-
-app.use( (req, res, next) => {
-  res.send('hello from express');
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "abcd",
+      title: "First server post",
+      content: "On my way from server"
+    },
+    {
+      id: "efgh",
+      title: "Second server post",
+      content: "On my way from server again"
+    }
+  ];
+  res.status(200).json({
+    message: "Posts retrieved successfully!",
+    posts: posts
+  });
 });
 
 
